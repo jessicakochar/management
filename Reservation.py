9c8a3f552d7578d66ddb7a4746ba9fdd0fa122cd
jessicakochar <81209119+jessicakochar@users.noreply.github.com>
2022-12-22 17:55:52 +0530

    Add files via upload

---
+++ Reservation.py
@@ -0,0 +1,70 @@
+from flask import *
+
+from Reservation_Logger import Reservation_Logger
+from ReservationLogger import ReservationLogger
+
+web_app = Flask("Hotel Management")
+r_log = Reservation_Logger()
+
+
+@web_app.route("/home_page")
+def index():
+    return render_template("home.html")
+
+
+@web_app.route("/add")
+def add_reservation_log():
+    return render_template("add_reservation_log.html")
+
+
+@web_app.route("/cancel")
+def cancel_reservation_log():
+    return render_template("delete_reservation_log.html")
+
+
+@web_app.route("/employee")
+def employee_login():
+    return render_template("employee_login.html")
+
+
+@web_app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@web_app.route("/logs")
+def view_all_logs():
+    reservation_obj = ReservationLogger()
+    sql = reservation_obj.fetch_sql_command()
+    rows = r_log.read(sql)
+    return render_template("view_all_logs.html", result=rows)
+
+
+@web_app.route("/save", methods=["POST"])
+def save_data():
+    print("Save Reservation Logs Executed...")
+
+    reservation_data_object = ReservationLogger(name=request.form['name'],
+                                                phone=int(request.form['phone']),
+                                                email=request.form['email'],
+                                                date_from=int(request.form['Reservation From']),
+                                                date_to=int(request.form['Reservation To']),
+                                                room_num=int(request.form['Number of Rooms']),
+                                                room_type=request.form['Type of Room'],
+                                                meals=request.form['Meals'],
+                                                comments=request.form['Comments'],)
+
+    print(reservation_data_object)
+    sql = reservation_data_object.insert_sql_command()
+    print("SQL IS:", sql)
+    r_log.write(sql)
+
+    return "Reservation is done :) \n Enjoy your stay!!!"
+
+
+def main():
+    web_app.run()
+
+
+if __name__ == '__main__':
+    main()
